3c51ea76b0bfa9919fdad9e11e7d0a81435c5698
K-JC <kellytolfts93@hotmail.com>
2023-08-08 15:38:27 +0000

    add context.js axios

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://rest-framework-1160aadccddd.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] ='multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
