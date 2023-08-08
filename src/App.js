a7b68d8b747472935162cfd85e0a65717f104015
K-JC <kellytolfts93@hotmail.com>
2023-08-08 14:55:59 +0000

    logged user access, user can now log in, can sign up and sign in. Continue to  add interceptors

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import './api/axiosDefaults';
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForms";
+
 
 function App() {
+
   return (
-    <div className="{styles.App}">
+    <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home Page</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
-  
       </Container>
     </div>
   );
---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://rest-framework-1160aadccddd.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] ='multipart/form-data'
+axios.defaults.withCredentials = true
